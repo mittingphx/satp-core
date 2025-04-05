28c4c18977bf9f97b5c3079d3ae98b1d9a9e7497
Scott Mitting <mittingphx@gmail.com>
2025-04-04 18:49:50 -0700

    Update encryption.py

--- flask_server/encryption.py
+++ flask_server/encryption.py
@@ -1,1 +1,15 @@
-# Encryption utils go here
+from cryptography.fernet import Fernet
+
+def generate_key():
+    return Fernet.generate_key()
+
+def get_fernet(key):
+    return Fernet(key)
+
+def encrypt_data(data, key):
+    f = get_fernet(key)
+    return f.encrypt(data.encode()).decode()
+
+def decrypt_data(token, key):
+    f = get_fernet(key)
+    return f.decrypt(token.encode()).decode()
